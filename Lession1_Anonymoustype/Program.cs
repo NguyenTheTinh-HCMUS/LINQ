9b3619766f5cf544fe21ee78ba5304facfeed514
Nguyễn Thế Tình <37909676+NguyenTheTinh-HCMUS@users.noreply.github.com>
2020-08-04 23:43:53 +0700

    commit lesson 2

---
+++ Lession1_Anonymoustype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Lession1_Anonymoustype
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // create a anonymous type  with use var keyword
+            var test = new { a = "Nguyen Van A" };
+            //test.a = "sjjss"; ===> error bc it only used to be read and ot change value
+        }
+    }
+}
