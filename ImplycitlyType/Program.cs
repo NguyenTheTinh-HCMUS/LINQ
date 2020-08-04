3a283b1bed0f522da99e79c51a792bd58d3db968
Nguyễn Thế Tình <37909676+NguyenTheTinh-HCMUS@users.noreply.github.com>
2020-08-05 00:11:14 +0700

    commit lession 3

---
+++ ImplycitlyType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ImplycitlyType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare implicitly type variable 
+            var a = 100;
+            // not declare when not initial for it
+            //var b;
+
+            // not allowed to declare multiple var in asigle statement
+                // var c,d,e;
+
+            // not allowed to use a null value in implicitly typed variable
+                //var f = null;
+            
+
+        }
+    }
+}
