58dc09980a080f23bdcfad8b120fdd388cfffd60
Nguyễn Thế Tình <37909676+NguyenTheTinh-HCMUS@users.noreply.github.com>
2020-08-05 17:38:40 +0700

    Commit lession 3

---
+++ Lesson3_ExentionMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson3_ExentionMethod
+
+{
+    static class Mystring
+    {
+        public static int WordCount(this string str)
+        {
+            string[] strArray = str.Split(' ');
+            return strArray.Length;
+
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string str = "Hello World";
+            int a= str.WordCount();
+            Console.WriteLine(a);
+        }
+    }
+}
